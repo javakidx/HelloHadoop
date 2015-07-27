44632abea075f1fa6f8dd7ba8968e768f6a58bf3
javakidx <javakidx@gmail.com>
2015-07-28 01:04:34 +0800

    oldAPI without tokenizer

---
+++ src/main/java/idv/jk/hellohadoop/WordCountNewAPI.java
@@ -0,0 +1,72 @@
+package idv.jk.hellohadoop;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+/**
+ * Created by javakid on 2015/7/28.
+ */
+public class WordCountNewAPI
+{
+    public static class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable>
+    {
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
+        {
+            StringTokenizer tokenizer = new StringTokenizer(value.toString(), " ");
+
+            while(tokenizer.hasMoreTokens())
+            {
+                context.write(new Text(tokenizer.nextToken()), new IntWritable(1));
+            }
+        }
+    }
+
+    public static class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable>
+    {
+        @Override
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
+        {
+            int sum = 0;
+            for(IntWritable value : values)
+            {
+                sum += value.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException
+    {
+        Job job = Job.getInstance(new Configuration());
+        job.setJarByClass(WordCountNewAPI.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setMapperClass(MyMapper.class);
+        job.setReducerClass(MyReducer.class);
+
+        job.setInputFormatClass(TextInputFormat.class); //the package
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
